5a22c5b347a563b3f512407af6adb3724dd00c75
Silas <si@Silas-MacBook-Pro.local>
2022-02-14 10:22:06 -0700

    add broken test'

--- example.py
+++ example.py
@@ -1,5 +1,11 @@
 def test_add():
    assert add(1,1) == 2
 
+def test_sub():
+    assert subtract(1,1) == 0
+
+def subtract(val1, val2):
+    return val1 + val2
+
 def add(val1, val2):
     return val1 + val2
